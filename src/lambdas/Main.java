773870cf46403329f7cd2fd7541bb39c77e6aecc
MarWeUMR <marcus.wallau@googlemail.com>
2019-02-05 15:41:18 +0100

    code reduction

--- src/lambdas/Main.java
+++ src/lambdas/Main.java
@@ -5,28 +5,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         System.out.println(
                 getBetterElement("Greedo", "Han", (x, y) -> x.compareTo(y) > 0)
         );
 
         System.out.println(
-                getBetterElement(20, 10, (x, y) -> x.compareTo(y) > 0)
+                getBetterElement(2, 2, (x, y) -> x.compareTo(y) > 0)
         );
 
 
     }
 
     public static <T> T getBetterElement(T e1, T e2, BiPredicate<T> pred) {
-
-        boolean b = pred.test(e1, e2);
-
-        if (b == false) {
-            return e2;
-        } else {
-            return e1;
-        }
-
-
+        return pred.test(e1, e2) ? e1 : e2;
     }
 }
